1ea669d32ed14073a95667b6c1f0fc35b6a9a1e9
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-13 19:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(ropes) {
+  let cost = 0;
+  ropes.sort((a, b) => a - b);
+
+  while (ropes.length > 1) {
+    let first = ropes.shift();
+    let second = ropes.shift();
+    let combined = first + second;
+    cost += combined;
+    ropes.push(combined);
+    ropes.sort((a, b) => a - b);
+  }
+
+  return cost;
 }
 
+
 module.exports=mincost;
+
+
+
